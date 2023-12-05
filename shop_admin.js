09209a456902708282c9b1affe07f47189c2852b
BanSugyeong <sugyeong0425@gmail.com>
2023-12-05 11:42:48 +0900

    2023-12-05/컬러박스 수정

--- shop_admin.js
+++ shop_admin.js
@@ -1,10 +1,20 @@
 // 새 상품 추가 함수
 function addProduct() {
     const productName = prompt('새 상품의 이름을 입력하세요:');
+    const productDescription = prompt('새 상품의 설명을 입력하세요:');
+    const productImage = prompt('새 상품의 이미지 URL을 입력하세요:');
     const productPrice = prompt('새 상품의 가격을 입력하세요:');
 
-    // 실제로는 서버에 상품 추가 요청을 보내야 하지만 여기서는 간단하게 배열에 추가
-    products.push({ id: products.length + 1, name: productName, price: parseInt(productPrice) });
+    // 새로운 상품 추가
+    const newProduct = {
+        id: products.length + 1,
+        name: productName,
+        description: productDescription,
+        image: productImage,
+        price: parseInt(productPrice)
+    };
+
+    products.push(newProduct);
 
     // 상품 목록 갱신
     displayProducts();
